ea9997812ee056a8c8ea3cf35eb7b80526f54528
darcher- <dylarcher@gmail.com>
2025-06-13 17:56:22 -0400

    fix: imports "done" from node:test

--- src/browser/debounce.spec.js
+++ src/browser/debounce.spec.js
@@ -1,4 +1,4 @@
-import { describe, it, beforeEach } from 'node:test';
+import { describe, it, beforeEach, done } from 'node:test';
 import assert from 'node:assert/strict';
 import { debounce } from './debounce.js';
 
